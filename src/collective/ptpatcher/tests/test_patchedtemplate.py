3c290fdb2fb2c5adf57118380f2d60c8f1c3ce30
Alessandro Pisa <alessandro.pisa@gmail.com>
2016-11-22 00:58:14 +0100

    Test ptpatcher

--- src/collective/ptpatcher/tests/test_patchedtemplate.py
+++ src/collective/ptpatcher/tests/test_patchedtemplate.py
@@ -3,7 +3,9 @@ from collective.ptpatcher.base import BasePatchedPTFile
 from collective.ptpatcher.testing import COLLECTIVE_PTPATCHER_INTEGRATION_TESTING  # noqa
 from pkg_resources import resource_filename
 from plone import api
+from Products.CMFPlone.browser.admin import Overview
 from Products.Five import BrowserView
+from pyquery import PyQuery as pq
 
 import unittest
 
@@ -34,6 +36,34 @@ class DummyView(BrowserView):
     )
 
 
+class DummyXMLPatchedPTFile(BasePatchedPTFile):
+    ''' Dummy pathcer for testing purpose
+    '''
+    def create_target(self):
+        ''' A patcher that capitalizes text
+        '''
+        obj = pq(filename=self.original)
+        header = obj('h1')[0]
+        pq(header).after('<p>${python:len(sites)} sites created</p>')
+        return str(obj)
+
+
+class PatchedOverview(Overview):
+    ''' Patched Plone overview
+    '''
+
+    index = DummyXMLPatchedPTFile(
+        original=resource_filename(
+            'Products.CMFPlone.browser',
+            'templates/plone-overview.pt',
+        ),
+        target=resource_filename(
+            'collective.ptpatcher',
+            'tests/ptpatcher/plone-overview.pt',
+        ),
+    )
+
+
 class TestSetup(unittest.TestCase):
     '''Test that collective.ptpatcher is properly installed.'''
 
@@ -41,10 +71,11 @@ class TestSetup(unittest.TestCase):
 
     def setUp(self):
         '''Custom shared utility setup for tests.'''
+        self.app = self.layer['app']
         self.portal = self.layer['portal']
         self.request = self.layer['request']
 
-    def test_patch(self):
+    def test_dummy_patch(self):
         '''Test that ICollectivePtpatcherLayer is registered.'''
         view = api.content.get_view(
             'test-ptpatcher',
@@ -54,4 +85,15 @@ class TestSetup(unittest.TestCase):
         self.assertEqual(
             view(),
             u'Hello world!\n',
+        )
+
+    def test_xml_patch(self):
+        view = api.content.get_view(
+            'test-patched-plone-overview',
+            self.app,
+            self.request.clone(),
+        )
+        self.assertIn(
+            u'<p>1 sites created</p>',
+            view(),
         )
