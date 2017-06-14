eb5a18525c74a4153da158eb1875d3150e48fa09
ale-rt <alessandro.pisa@gmail.com>
2017-06-14 09:09:25 +0200

    Fix dependencies

--- setup.py
+++ setup.py
@@ -38,10 +38,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'plone.api',
-        'Products.GenericSetup>=1.8.2',
         'setuptools',
-        'z3c.jbot',
+        'zpretty',
     ],
     extras_require={
         'test': [
